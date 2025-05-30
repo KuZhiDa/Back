a73c82632257a64b1f193d6eff1b6faed2132614
dimad <daminovd2005@gmail.com>
2025-05-31 00:25:48 +0300

    first commit

--- fullstack_django/settings.py
+++ fullstack_django/settings.py
@@ -94,6 +94,7 @@ CORS_ALLOWED_ORIGINS = [
     "http://localhost:3000",
     "http://repaircounting.ru",
     "https://back-buo4.onrender.com"
+    "https://683a21195bf7e27523b326e1--buildrabota"
 ]
 CORS_ALLOW_CREDENTIALS = True
 CSRF_TRUSTED_ORIGINS = ["https://back-buo4.onrender.com"]
